b90c0f138d0e9a9ce0a42fae1303b962468e80f8
Lara Sitruk <larasitruk@gmail.com>
2021-06-11 11:28:26 +0200

    adding repo

---
+++ main.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+// document.querySelector('#app').innerHTML = `
+//   <h1>${movueTitle}!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
+/**
+ * 
+ * 1. Read https://developers.themoviedb.org/3/getting-started/introduction and get API KEY
+ * 2. Find in the docs, the endpoint that might return movies
+ * 3. Use fetch to retrieve this data https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+ * 4. Console log the movie data (remember to open the developer console in the browser)
+ */
